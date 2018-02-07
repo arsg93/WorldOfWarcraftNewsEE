ede39175498e6da292559e9f6c9ed090994e3508
Andres <admin@admin-PC>
2018-02-07 18:23:06 +0100

    Se han añadido las entities y las session beans.

---
+++ WorldOfWarcraftNewsEE-ejb/src/java/com/wownews/entities/News.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wownews.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author admin
+ */
+@Entity
+@Table(name = "news")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "News.findAll", query = "SELECT n FROM News n")
+    , @NamedQuery(name = "News.findById", query = "SELECT n FROM News n WHERE n.id = :id")
+    , @NamedQuery(name = "News.findBySlug", query = "SELECT n FROM News n WHERE n.slug = :slug")
+    , @NamedQuery(name = "News.findByTitle", query = "SELECT n FROM News n WHERE n.title = :title")
+    , @NamedQuery(name = "News.findByDescription", query = "SELECT n FROM News n WHERE n.description = :description")
+    , @NamedQuery(name = "News.findByDate", query = "SELECT n FROM News n WHERE n.date = :date")
+    , @NamedQuery(name = "News.findByUsername", query = "SELECT n FROM News n WHERE n.username = :username")
+    , @NamedQuery(name = "News.findByImage", query = "SELECT n FROM News n WHERE n.image = :image")})
+public class News implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    @Size(max = 30)
+    @Column(name = "slug")
+    private String slug;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "title")
+    private String title;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10000)
+    @Column(name = "description")
+    private String description;
+    @Column(name = "date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "username")
+    private String username;
+    @Size(max = 50)
+    @Column(name = "image")
+    private String image;
+
+    public News() {
+    }
+
+    public News(Long id) {
+        this.id = id;
+    }
+
+    public News(Long id, String title, String description, String username) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof News)) {
+            return false;
+        }
+        News other = (News) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.wownews.entities.News[ id=" + id + " ]";
+    }
+    
+}
---
+++ WorldOfWarcraftNewsEE-ejb/src/java/com/wownews/sessionBeans/NewsFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wownews.sessionBeans;
+
+import com.wownews.entities.News;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author admin
+ */
+@Stateless
+public class NewsFacade extends AbstractFacade<News> {
+
+    @PersistenceContext(unitName = "WorldOfWarcraftNewsEE-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public NewsFacade() {
+        super(News.class);
+    }
+    
+}
